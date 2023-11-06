ddcab86b87ccbca3326f94396506c2c52020686d
spopONFIRE <fihdospop429@gmail.com>
2023-11-06 15:44:25 -0300

    Ultimo commit

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ const path = require("path")
 
 const app = express()
 
-const caminhoBase = path.jdin(__dirname, "tamplates")
+const caminhoBase = path.join(__dirname, "tamplates")
 
-app.get('/usuarios')
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, reposta) => {
+    const name = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`O email do usuario é: ${email}`)
+})
+
+app.get('/cadastrar', (requisicao, reposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
+app.get('/usuarios/:id', (requisicao, reposta) => {
+    const id = requisicao.parans.id
+
+    console.log(`acessando dados do usuário ${id}`)
+
+    reposta.sandFile(`${caminhoBase}/usuario.html`)
+})
 
 app.get('/', (requisicao, reposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
